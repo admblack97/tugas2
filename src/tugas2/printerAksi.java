34ff068e60a3244f4c299cd236e486e993db5092
admblack97 <admblack97@gmail.com>
2018-03-31 14:02:20 +0800

    instance object

--- src/tugas2/printerAksi.java
+++ src/tugas2/printerAksi.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tugas2;
 
-/**
- *
- * @author Adam Morseleno
- */
 public class printerAksi {
-    
+    public static void main(String[] args) {
+        printer p1 = new printer();
+        printer p2 = new printer();
+        printer p3 = new printer();
+    }
 }
