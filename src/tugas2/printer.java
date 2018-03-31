1837bebf96edaedffaaccae2461372a3394ae822
admblack97 <admblack97@gmail.com>
2018-03-31 10:01:04 +0800

    membuat atribut dan behavior

--- src/tugas2/printer.java
+++ src/tugas2/printer.java
@@ -1,5 +1,18 @@
 package tugas2;
 
 public class printer {
+    //atribut
+    String merek;
+    Integer harga;
+    double jumlah_item;
+    
+    //behavior
+    void beli() {
+        System.out.println("|=======Pembelian Printer=======|");
+        System.out.println(" Merek Printer : "+merek);
+        System.out.println(" Harga Printer : "+harga);
+        System.out.println(" Harga Printer : "+jumlah_item);
+        System.out.println("|===============================|");
+    }
     
 }
