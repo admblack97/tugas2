f8e507e5b091b42586aef7f026f3da5579877d5a
admblack97 <admblack97@gmail.com>
2018-03-31 14:03:36 +0800

    mengisi nilai atribut

--- src/tugas2/printerAksi.java
+++ src/tugas2/printerAksi.java
@@ -5,5 +5,12 @@ public class printerAksi {
         printer p1 = new printer();
         printer p2 = new printer();
         printer p3 = new printer();
+        
+        System.out.println("|=======Pembelian Printer=======|");
+        p1.merek="Epson L605.";
+        p1.harga=2500000;
+        p1.jumlah_item=1;
+        System.out.println("|===============================|");
+     
     }
 }
