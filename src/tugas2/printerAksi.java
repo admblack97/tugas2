f0081333b7b4ed8bca7c7dc75fea8bf565e81215
admblack97 <admblack97@gmail.com>
2018-03-31 14:22:02 +0800

    memanggil method/behavior

--- src/tugas2/printerAksi.java
+++ src/tugas2/printerAksi.java
@@ -3,26 +3,24 @@ package tugas2;
 public class printerAksi {
     public static void main(String[] args) {
         printer p1 = new printer();
-        printer p2 = new printer();
-        printer p3 = new printer();
-        
         System.out.println("|=======Pembelian Printer=======|");
         p1.merek="Epson L605.";
         p1.harga=2500000;
         p1.jumlah_item=1;
-        System.out.println("|===============================|"+"\n");
+        p1.beli();
         
+        printer p2 = new printer();
         System.out.println("|=======Pembelian Printer=======|");
         p2.merek="Epson L310.";
         p2.harga=1500000;
         p2.jumlah_item=1;
-        System.out.println("|===============================|"+"\n");
+        p2.beli();
         
+        printer p3 = new printer();
         System.out.println("|=======Pembelian Printer=======|");
         p3.merek="Epson L600.";
         p3.harga=6000000;
         p3.jumlah_item=1;
-        System.out.println("|===============================|"+"\n");
-     
+        p3.beli();
     }
 }
